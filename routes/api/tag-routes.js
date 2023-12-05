8ffc9e0ad4c42ed542f78260442c60a629b43c4c
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2023-12-05 22:43:47 +0000

    completed tag-routs.js partially, just need to make sure to include associated data for the get routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,33 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.getAll();
+  return res.json(Tag);
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.get(req.params.id);
+  return res.json(Tag);
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body);
+  return res.json(Tag);
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body,(req.params.id));
+  return res.json(Tag);
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.delete(req.params.id);
+  return res.json(Tag);
 });
 
 module.exports = router;
