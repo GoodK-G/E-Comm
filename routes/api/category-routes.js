13e537324b40dd4a2d546f21b6188a4184cabc20
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2023-12-06 19:44:17 +0000

    Added routes for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,33 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.getAll();
+  return res.json(Category);
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.get(req.params.id);
+  return res.json(Category);
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body);
+  return res.json(Category);
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body(req.params.id));
+  return res.json(Category);
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.delete(req.params.id);
+  return res.json(Category);
 });
 
 module.exports = router;
